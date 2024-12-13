4c9e3c5c90603203d4852f54e6686208d21f9489
Bekansm <skakovbekzhan@gmail.com>
2024-12-14 01:34:38 +0500

    figure type logic full rework

--- script.js
+++ script.js
@@ -1,41 +1,72 @@
-const figure = document.querySelector(".figure");
+const windowElement = document.querySelector(".windowElement");
 const figureTypes = document.querySelectorAll(".type");
 const colorTypes = document.querySelectorAll(".color");
 const inputButton = document.querySelector(".addText");
 const inputText = document.querySelector("input");
-const content = document.querySelector("p");
+let content = document.createElement("p");
 const resetButton = document.querySelector(".reset");
+let currentFigure = null;
+
 //Выбор фигуры
-figureTypes.forEach(type => {
-    type.addEventListener("click", (e) =>{ figureType(e.target.textContent);
-     })
+figureTypes.forEach((type) => {
+	type.addEventListener("click", (e) => {
+		figureType(e.target.textContent);
+	});
 });
 //Выбор цвета
-colorTypes.forEach(type => {
-    type.addEventListener("click", (e) =>{  
-        figure.style.backgroundColor = figure.classList.contains("hidden") ? alert("Сперва выберите фигуру") : colors[e.target.textContent].hex;
-     })
+colorTypes.forEach((type) => {
+	type.addEventListener("click", (e) => {
+		if (currentFigure === null) {
+			alert("Сперва выберите фигуру");
+		} else {
+			currentFigure.style.backgroundColor = colors[e.target.textContent].hex;
+		}
+	});
 });
 //Изменение текста
-inputButton.addEventListener("click", (e) => {  
-    const textValue = inputText.value.trim();
-    content.textContent = textValue!== "" ? inputText.value : alert("Необходимо ввести текст");
-})
+inputButton.addEventListener("click", () => {
+	const textValue = inputText.value.trim();
+	if (currentFigure === null) {
+		alert("Сперва выберите фигуру");
+	} else if (textValue === "") {
+		alert("Необходимо ввести текст");
+	} else {
+		content.textContent = textValue;
+	}
+});
 //Сброс
 resetButton.addEventListener("click", (e) => {
-    figure.classList.add("hidden");
-    content.textContent = "";
-    inputText.value = "";
-})
+	if (currentFigure !== null) {
+		currentFigure.remove();
+		content.textContent = "";
+		inputText.value = "";
+		currentFigure = null;
+	}
+});
 //Функция для изменения фигуры
-function figureType(type){
-    figure.classList.remove("hidden");
-    figure.style.borderRadius = type === "Круг" ? "50%" : "0%";
+function figureType(type) {
+	const newRadius = type === "Круг" ? "50%" : "0%";
+	if (
+		currentFigure !== null &&
+		currentFigure.style.borderRadius === newRadius
+	) {
+		return;
+	}
+
+	currentFigure !== null ? currentFigure.remove() : null;
+
+	const figure = document.createElement("div");
+	figure.classList.add("figure");
+	figure.style.borderRadius = newRadius;
+	windowElement.appendChild(figure);
+
+	currentFigure = figure;
+	currentFigure.appendChild(content);
 }
 
 const colors = {
-    "Красный": { hex: "#E32636" },
-    "Черный": { hex: "#000000" },
-    "Зеленый": { hex: "#138808" },
-    "Синий": { hex: "#1560BD" }
+	Красный: { hex: "#E32636" },
+	Черный: { hex: "#000000" },
+	Зеленый: { hex: "#138808" },
+	Синий: { hex: "#1560BD" },
 };
